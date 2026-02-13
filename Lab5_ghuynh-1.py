dfd867fbec548d2e32b804b1dd8d3f53e8d62f28
Gaven <ghuynh@student.cscc.edu>
2026-02-12 23:02:09 -0500

    Docstring

---
+++ Lab5_ghuynh-1.py
@@ -0,0 +1,9 @@
+"""
+Dice Rolling Terms
+Gaven Huynh
+This code simulates throwing a pair of die.
+It will display the value of each die, the sum, as well as the correlating term.
+The code will run until the user decides to quit the program.
+February 12, 2026
+"""
+
