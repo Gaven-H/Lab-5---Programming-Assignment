b46ba3bfcca67c131a29d5d85b093460d4a0d095
Gaven <ghuynh@student.cscc.edu>
2026-02-12 23:28:59 -0500

    Condition: Boxcars

--- Lab5_ghuynh-1.py
+++ Lab5_ghuynh-1.py
@@ -47,3 +47,6 @@ def dice_roll_term(die1, die2):
     
     elif (die1 == 6 and die2 == 5) or (die1 == 5 and die2 == 6):
         return "Six Five no Jive"
+    
+    elif die1 == 6 and die2 == 6:
+        return "Boxcars"
